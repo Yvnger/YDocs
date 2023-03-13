8db2c36d3c9b1327aa287e1c1df46427e0930314
YVNGER <yvnger@gmail.com>
2023-03-13 10:28:55 +0400

    Second commit

--- woodocs.php
+++ woodocs.php
@@ -33,7 +33,7 @@ function add_custom_order_column_content($column)
     if ('docs' === $column) {
         $order_id = $post->ID;
         $order = wc_get_order($order_id);
-        echo '<a href="' . wp_nonce_url(admin_url('admin-ajax.php?action=print_order&order_id=' . $order_id), 'print_order_' . $order_id) . '">' . __('Print Order', 'woodocs') . '</a>';
+        echo '<a class="button" href="' . wp_nonce_url(admin_url('admin-ajax.php?action=print_order&order_id=' . $order_id), 'print_order_' . $order_id) . '" target="_blank">' . __('Распечатать заказ', 'woodocs') . '</a>';
     }
 }
 
@@ -56,13 +56,140 @@ function print_order()
         wp_die(__('Invalid order ID', 'woodocs'));
     }
 
-    // Создаем PDF документ
+    // Создаем экземпляр класса TCPDF
     $pdf = new TCPDF();
-    $pdf->AddPage();
-    $pdf->SetFont('helvetica', '', 12);
-    $pdf->Write(0, 'Hello, World!' . $order);
 
-    // Выводим PDF документ
-    $pdf->Output('order-' . $order_id . '.pdf', 'D');
+    // Создаем PDF документ
+    $pdf->AddPage();
+
+    // Устанавливаем метаданные документа
+    $pdf->SetCreator('Mir-Chemodanov.ru');
+    $pdf->SetAuthor('Mir-Chemodanov.ru');
+    $pdf->SetTitle('Заказ ' . $order_id);
+
+    // Добавляем заголовок
+    $pdf->SetFont('dejavusans', '', 10, '', true, 'UTF-8');
+    // $pdf->Write(10, 'Order #' . $order_id, '', 0, 'C');
+
+    // Установка отступов от края страницы
+    $pdf->SetMargins(10, 10, 10);
+
+    // Добавляем дату документа
+    $pdf->Cell(0, 10, $order->get_date_created()->format('d.m.Y, H:i'), 0, 1, 'L');
+
+    $pdf->SetFont('dejavusans', '', 16, '', true, 'UTF-8');
+    $pdf->SetTextColor(33, 47, 73);
+
+    $title = "Mir-Chemodanov.ru - Оптовая продажа багажа №1 по России";
+    $subtitle = "г.Москва Тел.: +7 (925) 335-74-10 E-mail: mir­chemodanov@mail.ru";
+
+    $pdf->MultiCell(95, 10, $title, 0, 'L', false, 1, '', '', true, 0, false, true, 0, 'T', false);
+
+    $pdf->SetFont('dejavusans', '', 10, '', true, 'UTF-8');
+    $pdf->MultiCell(95, 10, $subtitle, 0, 'L', false, 0, 10, 35, true, 0, false, true, 0, 'T', false);
+
+    /** 
+     * Добавляем логотип
+     * Путь, отступ слева, отступ сверху, масштаб, масштаб по вертикали
+     */
+    $pdf->Image(plugin_dir_path(__FILE__) . 'assets/logo.png', 110, 20, 90);
+
+    // Рисуем разделительную линию
+    $pdf->SetDrawColor(0, 0, 0);
+    $pdf->SetLineWidth(0.5);
+    $pdf->Line(10, 50, 200, 50);
+
+    // Добавляем информацию о заказе
+    $pdf->SetFont('dejavusans', '', 10, '', true, 'UTF-8');
+
+    $fullname = $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
+
+    $address['country'] = $order->get_billing_country();
+    $address['state'] = $order->get_billing_state();
+    $address['city'] = $address['state'] . ', ' . $order->get_billing_city();
+    $address['postcode'] = $order->get_billing_postcode();
+    $address['street'] = $order->get_billing_address_1() . ' ' . $order->get_billing_address_2();
+
+    $numnum = get_post_meta( $order_id, '_saphali_lite_invoice_no', true );
+
+    $margin_top = 8;
+    $margin_left = 50;
+    $font_size = 6;
+    $pdf->Ln(20);
+
+    $pdf->SetFont('dejavusans', 'B', $font_size, '', true, 'UTF-8');
+    $pdf->Cell(0, $margin_top, 'Содержание заказа', 0, 1);
+
+    $pdf->Cell($margin_left, $margin_top, 'ФИО:', 0);
+
+    $pdf->SetFont('dejavusans', '', $font_size, '', true, 'UTF-8');
+    $pdf->Cell(0, $margin_top, $fullname, 0, 1);
+
+    $pdf->SetFont('dejavusans', 'B', $font_size, '', true, 'UTF-8');
+    $pdf->Cell($margin_left, $margin_top, 'Адрес:', 0);
+
+    $pdf->SetFont('dejavusans', '', $font_size, '', true, 'UTF-8');
+    $pdf->Cell(0, $margin_top, $address['street'], 0, 1);
+
+    $pdf->SetFont('dejavusans', 'B', $font_size, '', true, 'UTF-8');
+    $pdf->Cell($margin_left, $margin_top, 'Город:', 0);
+
+    $pdf->SetFont('dejavusans', '', $font_size, '', true, 'UTF-8');
+    $pdf->Cell(0, $margin_top, $address['city'], 0, 1);
+
+    $pdf->SetFont('dejavusans', 'B', $font_size, '', true, 'UTF-8');
+    $pdf->Cell($margin_left, $margin_top, 'Страна:', 0);
+
+    $pdf->SetFont('dejavusans', '', $font_size, '', true, 'UTF-8');
+    $pdf->Cell(0, $margin_top, $address['country'], 0, 1);
+
+        // Рисуем разделительную линию
+        $pdf->SetDrawColor(0, 0, 0);
+        $pdf->SetLineWidth(0.1);
+        $pdf->Line(10, 120, 200, 120);
+
+    // Добавляем информацию о товарах в заказе
+    $pdf->Ln(20);
+    $pdf->Cell(50, 10, 'Product', 0);
+    $pdf->Cell(50, 10, 'Quantity', 0);
+    $pdf->Cell(50, 10, 'Price', 0);
+    $pdf->Cell(0, 10, 'Total', 0, 1);
+
+    foreach ($order->get_items() as $item) {
+        $product = $item->get_product();
+        $pdf->Cell(50, 10, $product->get_name(), 0);
+        $pdf->Cell(50, 10, $item->get_quantity(), 0);
+        $pdf->Cell(50, 10, wc_price($product->get_price()), 0);
+        $pdf->Cell(0, 10, wc_price($item->get_total()), 0, 1);
+    }
+
+    // Добавляем информацию о доставке
+    $pdf->Ln(20);
+    $pdf->Cell(0, 10, 'Shipping Information', 0, 1);
+    $pdf->Cell(50, 10, 'Method:', 0);
+    $pdf->Cell(0, 10, $order->get_shipping_method(), 0, 1);
+    $pdf->Cell(50, 10, 'Address:', 0);
+    $pdf->Cell(0, 10, $order->get_shipping_address_1() . ' ' . $order->get_shipping_address_2(), 0, 1);
+    $pdf->Cell(50, 10, 'City:', 0);
+    $pdf->Cell(0, 10, $order->get_shipping_city(), 0, 1);
+    $pdf->Cell(50, 10, 'State:', 0);
+    $pdf->Cell(0, 10, $order->get_shipping_state(), 0, 1);
+    $pdf->Cell(50, 10, 'Zip Code:', 0);
+    $pdf->Cell(0, 10, $order->get_shipping_postcode(), 0, 1);
+    $pdf->Cell(50, 10, 'Country:', 0);
+    $pdf->Cell(0, 10, $order->get_shipping_country(), 0, 1);
+
+    // Добавляем общую стоимость заказа
+    $pdf->Ln(20);
+    $pdf->Cell(0, 10, 'Order Total', 0, 1);
+    $pdf->Cell(50, 10, 'Subtotal:', 0);
+    $pdf->Cell(0, 10, wc_price($order->get_subtotal()), 0, 1);
+    $pdf->Cell(50, 10, 'Shipping:', 0);
+    $pdf->Cell(0, 10, wc_price($order->get_shipping_total()), 0, 1);
+    $pdf->Cell(50, 10, 'Total:', 0);
+    $pdf->Cell(0, 10, wc_price($order->get_total()), 0, 1);
+
+    // Выводим документ в браузер или сохраняем его в файл
+    $pdf->Output('order_' . $order_id . '.pdf', 'I');
     exit;
 }
